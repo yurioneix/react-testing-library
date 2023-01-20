dc20ecdb162045d02cc57a91aacedcd102fe8c9d
yurioneix <yurioneix18@gmail.com>
2023-01-20 16:48:40 -0300

    Requisito 2: testa se a página contém um h2 com o texto 'About Pokedéx'

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helper/renderWithRouter';
+import { About } from '../pages';
+
+describe('Teste o componente <About.js />', () => {
+  it('Teste se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const title = screen.getByRole('heading', { level: 2 });
+    expect(title).toHaveTextContent('About Pokédex');
+  });
+});
