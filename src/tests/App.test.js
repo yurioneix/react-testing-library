aaa52faee8f1ea0c5a9fb017fadd725c9822143a
yurioneix <yurioneix18@gmail.com>
2023-01-20 15:37:37 -0300

    Requisito 1: testa os links de navegação e se a página é redirecionada para a Home

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../helper/renderWithRouter';
+
+describe('Teste o componente <App.js />', () => {
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByText(/Home/i);
+    expect(linkHome).toBeInTheDocument();
+
+    const linkAbout = screen.getByText(/About/i);
+    expect(linkAbout).toBeInTheDocument();
+
+    const linkPokemon = screen.getByText(/Favorite Pokémon/i);
+    expect(linkPokemon).toBeInTheDocument();
+  });
+
+  it('Teste se a aplicação é redirecionada para a página inicial', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkHome = screen.getByText(/Home/i);
+    userEvent.click(linkHome);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+});
